c57d01cf64d4846a0fb06359b2aee0fe3c33845e
kannan-kv <v_kannan@inturn.co>
2024-06-06 19:27:37 +0530

    feat: add methods for stepsstore struct

--- common.go
+++ common.go
@@ -5,3 +5,31 @@ type Step struct {
 	Progress string
 	Error    error
 }
+type StepsStore []Step
+
+func (s StepsStore) StartNextStage() {
+	for _, step := range s {
+		if step.Progress == "not_started" {
+			step.Progress = "in_progress"
+			break
+		}
+	}
+}
+
+func (s StepsStore) FailPresentStage() {
+	for _, step := range s {
+		if step.Progress == "in_progress" {
+			step.Progress = "failed"
+			break
+		}
+	}
+}
+
+func (s StepsStore) CompletePresentStage() {
+	for _, step := range s {
+		if step.Progress == "in_progress" {
+			step.Progress = "completed"
+			break
+		}
+	}
+}
